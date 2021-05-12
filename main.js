3f3da8899f540d2dd0e40430304d53b6c1155dc4
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-05-12 17:30:20 +1000

    refactored setup to be more flexible

--- main.js
+++ main.js
@@ -12,22 +12,24 @@ function Pillars(object){
 }
 
 
-Pillars.prototype.setup = function () {
+Pillars.prototype.setup = function (option ,space) {
+
+    if (space === undefined) {space = 0}
 
     let index = this.dom.pillar.length;
     let spacing = 0;
 
     while(index--) {
         this.dom.pillar[index].style.position = this.pillarObj.position;
-        this.dom.pillar[index].style.left = `${this.pillarObj.left}px`;
         this.dom.pillar[index].style.width = `${this.pillarObj.width}px`;
         this.dom.pillar[index].style.height = `${this.pillarObj.height}px`;
         this.dom.pillar[index].style.backgroundColor = this.pillarObj.color;
 
-        this.dom.pillar[index].style.top = `${spacing}px`;
-        spacing += 110;
+        if (option !== undefined && option.includes('left')) { this.dom.pillar[index].style.left = `${spacing}px`; }
+        if (option !== undefined && option.includes('top')) { this.dom.pillar[index].style.top = `${spacing}px`; }
+        spacing += space;
     }
-
+    return;
 }
 
 Pillars.prototype.insertStyleRule = function (ruleText) {
@@ -41,14 +43,18 @@ Pillars.prototype.insertStyleRule = function (ruleText) {
     let sheet = sheets[sheets.length - 1]
     sheet.insertRule(ruleText, sheet.rules ? sheet.rules.length : sheet.cssRules.length)
 
+    return;
 }
 
 Pillars.prototype.transition = function (pixels) {
 
     switch (this.pillarObj.animation) {
         
-        case 'move':
-            this.move(pixels);
+        case 'moveAcross':
+            this.moveAcross(pixels);
+            break;
+        case 'moveDown':
+            this.moveDown(pixels);
             break;
         case 'stretch':
             this.stretch(pixels);
@@ -56,13 +62,25 @@ Pillars.prototype.transition = function (pixels) {
         case 'fade':
             this.fade();
             break;
+        default:
+            return console.error('No animation method was passed through');
     }
+
 }
 
-Pillars.prototype.move = function (pixels) {
+Pillars.prototype.moveAcross = function (pixels) {
 
     this.insertStyleRule(".pillar { animation: move 3s infinite alternate;}");
-    this.insertStyleRule(`@keyframes move { 0% { left: 0px; } 100% { left: ${pixels}px } }`);
+    this.insertStyleRule(`@keyframes move { 0% { left: ; } 100% { left: ${pixels}px } }`);
+    return;
+
+}
+
+Pillars.prototype.moveDown = function (pixels) {
+
+    this.insertStyleRule(".pillar { animation: move 3s infinite alternate;}");
+    this.insertStyleRule(`@keyframes move { 0% { top: ; } 100% { top: ${pixels}px } }`);
+    return;
 
 }
 
@@ -74,6 +92,7 @@ Pillars.prototype.delayPillar = function (passed) {
         this.dom.pillar[index].style.animationDelay = `${delay}s`;
         delay += passed
     }
+    return;
 }
 
 Pillars.prototype.everySecond = function (color) {
@@ -82,37 +101,38 @@ Pillars.prototype.everySecond = function (color) {
 
         this.dom.pillar[index++].style.backgroundColor = `${color}`;
     }
+    return;
 
 }
 
-// Pillars.prototype.stretch = function (pixels) {
+Pillars.prototype.stretch = function (pixels) {
 
-//     this.dom.stylesheet.style.setProperty( '--pillar-transition', 'stretch' )
+    this.dom.stylesheet.style.setProperty( '--pillar-transition', 'stretch' )
 
-//     let index = this.dom.pillar.length;
-//     for(let index = 0; index < this.dom.pillar.length; index++) {
+    let index = this.dom.pillar.length;
+    for(let index = 0; index < this.dom.pillar.length; index++) {
 
-//         this.dom.pillar[index].style.setProperty('--pillar-movement', `${pixels}px`);
-//         this.dom.pillar[index].style.animationDelay = `$0.{index}s`;
-//     }
-// }
+        this.dom.pillar[index].style.setProperty('--pillar-movement', `${pixels}px`);
+        this.dom.pillar[index].style.animationDelay = `$0.{index}s`;
+    }
+}
 
 
 const pillar = new Pillars({
     className   : 'pillar',
     position    : 'absolute',
-    width       : 300,
-    height      : 100,
+    width       : 50,
+    height      : 350,
     top         : 300,
     left        : 0,
-    color       : 'green',
-    animation   : 'move'
+    color       : 'blue',
+    animation   : 'moveAcross'
 });
 
-pillar.setup();
+pillar.setup(['left','top'], 10);
 
-pillar.transition(300, 1);
-pillar.everySecond('blue');
-pillar.delayPillar(2);
-//pillar.insertStyleRule(".pillar { animation: move 3s infinite alternate;}");
+pillar.transition(300);
+pillar.everySecond('red');
+pillar.delayPillar(.2);
+
 
